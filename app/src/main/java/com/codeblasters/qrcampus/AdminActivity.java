d4b87ca75ca2ea9d68a87b0d3831f929e64b6859
rohanvachhani <rohan.vachhani5@gmail.com>
2018-04-08 05:17:20 +0530

    added data to firebase

--- app/src/main/java/com/codeblasters/qrcampus/AdminActivity.java
+++ app/src/main/java/com/codeblasters/qrcampus/AdminActivity.java
@@ -3,19 +3,26 @@ package com.codeblasters.qrcampus;
 import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,8 +36,15 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Calendar;
+import java.util.Random;
+
+import me.ydcool.lib.qrmodule.encoding.QrGenerator;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -54,6 +68,9 @@ public class AdminActivity extends AppCompatActivity {
     String details_input;
     String QR_gen_string = "";
 
+    ImageView img;
+    Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,6 +87,8 @@ public class AdminActivity extends AppCompatActivity {
         month = calendar.get(Calendar.MONTH);
         day = calendar.get(Calendar.DAY_OF_MONTH);
         showDate(year, month + 1, day);
+
+        img = findViewById(R.id.imageView);
 
 
         title_input = findViewById(R.id.title);
@@ -151,6 +170,10 @@ public class AdminActivity extends AppCompatActivity {
 
     //If Access Granted gallery Will open
     private void callgalary() {
+        Resources res = getApplicationContext().getResources();
+
+        mImageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + res.getResourcePackageName(R.drawable.ic_launcher_background) + '/' + getResources().getResourceTypeName(R.drawable.no_image));
+        ;//bu default image (when user doesn't upload image)
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, GALLERY_INTENT);
@@ -173,16 +196,24 @@ public class AdminActivity extends AppCompatActivity {
 
     //submit button click
     public void makeQR(View view) {
+        try {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         Toast.makeText(this, "QR Code GEenerating..", Toast.LENGTH_SHORT).show();
 
         //taking data and make class object
         title_in = title_input.getText().toString().trim();
 
         details_input = details.getText().toString().trim();
+
+
         if (mUploadTask != null && mUploadTask.isInProgress()) {
             Toast.makeText(this, "Uploading is in progress", Toast.LENGTH_LONG).show();
         } else {
-            if (title_in != "") {
+            if (title_in != null) {
                 upload_file();
             } else {
                 Toast.makeText(this, "Please ADD Atleast a title of event or information", Toast.LENGTH_LONG).show();
@@ -195,27 +226,37 @@ public class AdminActivity extends AppCompatActivity {
     private void upload_file() {
         StorageReference filereference = storageReference.child(System.currentTimeMillis() + ".jpg");
 
-        mUploadTask = filereference.putFile(mImageUri)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        Toast.makeText(this, "mImagUri : " + mImageUri.toString(), Toast.LENGTH_LONG).show();
+        mUploadTask = filereference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
                     @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Handler handler = new Handler();
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                mProgressBar.setProgress(0);
-                            }
-                        }, 500);
-
-                        Toast.makeText(AdminActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
-
-                        String date = dateView.getText().toString();
-                        info upload = new info(title_in, taskSnapshot.getDownloadUrl().toString(), date, details_input);
-                        String uploadId = databaseReference.push().getKey();
-                        databaseReference.child(uploadId).setValue(upload);
-                        QR_gen_string = uploadId.toString();
+                    public void run() {
+                        mProgressBar.setProgress(0);
                     }
-                })
+                }, 500);
+
+                Toast.makeText(AdminActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+
+                String date = dateView.getText().toString();
+                String imge = taskSnapshot.getDownloadUrl().toString();
+                if (mImageUri == null) {
+                    Toast.makeText(getApplicationContext(), "Image URI inull", Toast.LENGTH_SHORT).show();
+
+                }
+                info upload = new info(title_in, imge, date, details_input);
+                String uploadId = databaseReference.push().getKey();
+                databaseReference.child(uploadId).setValue(upload);
+                QR_gen_string = uploadId;
+                Toast.makeText(AdminActivity.this, "ID: " + uploadId.toString(), Toast.LENGTH_LONG).show();
+                //call to display and save QR code
+                display_QR();
+
+            }
+
+        })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
@@ -230,5 +271,53 @@ public class AdminActivity extends AppCompatActivity {
                     }
                 });
 
+
     }
+
+    private void display_QR() {
+        Bitmap qrCode = null;
+        try {
+            qrCode = new QrGenerator.Builder()
+                    .content(QR_gen_string)
+                    .qrSize(300)
+                    .margin(2)
+                    .color(Color.BLACK)
+                    .bgColor(Color.WHITE)
+                    .ecc(ErrorCorrectionLevel.H)
+                    .encode();
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        img.setImageBitmap(qrCode);
+//        bmToJpg(qrCode);
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/req_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        Toast.makeText(this, myDir.toString(), Toast.LENGTH_SHORT).show();
+        File file = new File(myDir, fname);
+        // Log.i(TAG, "" + file);
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            qrCode.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            // out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void bmToJpg(Bitmap qrCode) {
+
+    }
+
+
 }
---
+++ app/src/main/java/com/codeblasters/qrcampus/QrResult.java
@@ -0,0 +1,15 @@
+package com.codeblasters.qrcampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codeblasters.qrcampus.R;
+
+public class QrResult extends WithManuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_result);
+    }
+}
