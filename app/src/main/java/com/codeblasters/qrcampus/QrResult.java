43d0abc761f4f42bf64a67b3557bc294c1ffa29b
rohanvachhani <rohan.vachhani5@gmail.com>
2018-04-08 10:21:29 +0530

    added qr scanner

--- app/src/main/java/com/codeblasters/qrcampus/QrResult.java
+++ app/src/main/java/com/codeblasters/qrcampus/QrResult.java
@@ -1,14 +1,18 @@
 package com.codeblasters.qrcampus;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.StorageReference;
 
 public class QrResult extends WithManuActivity {
@@ -17,7 +21,8 @@ public class QrResult extends WithManuActivity {
     private EditText title;
     private EditText date;
     private TextView details;
-
+    private info i;
+    private Uri imguri;
     private StorageReference storageReference;
     private DatabaseReference databaseReference;
 
@@ -34,12 +39,50 @@ public class QrResult extends WithManuActivity {
         String id = intent.getStringExtra("id");
         Toast.makeText(this, id, Toast.LENGTH_LONG).show();
 
-        databaseReference = FirebaseDatabase.getInstance().getReference("info");
+        i = new info();
+        databaseReference = FirebaseDatabase.getInstance().getReference("info").child(id);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                   /* Toast.makeText(getApplicationContext(),"getting the data...",Toast.LENGTH_SHORT).show();*/
+
+                String img_url = dataSnapshot.child("imageUri").getValue(String.class);
+                if(img_url.equals("no Image")){
+                    imguri = Uri.parse("android.resources://" + BuildConfig.APPLICATION_ID + "/" + R.drawable.no_image);
+                    imageView.setImageURI(imguri);
+                }
+                else{
+                    img_url = dataSnapshot.child("imageUri").getValue(String.class);
+                   // Glide.with(getApplicationContext()).load(img_url).into(imageView);
+
+                }
+                String title_ii = dataSnapshot.child("title").getValue(String.class);
+                String date_ii = dataSnapshot.child("date").getValue(String.class);
+                String details_ii = dataSnapshot.child("info").getValue(String.class);
+
+                title.setText(title_ii);
+                title.setFocusable(false);
+                title.setClickable(false);
+
+                date.setText(date_ii);
+                date.setFocusable(false);
+                date.setClickable(false);
+
+
+                details.setText(details_ii);
+                details.setFocusable(false);
+                details.setClickable(false);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
